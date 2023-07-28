721efde87aab5ee93b5543029f8d603e6cc02a10
GiSeong Lee <cj8928@gmail.com>
2023-07-28 18:24:50 +0900

    구단주명 검색 추가

--- src/component/SearchBar.tsx
+++ src/component/SearchBar.tsx
@@ -1,37 +1,72 @@
-import { Box, Button, Input, useTheme } from "@mui/material";
+import { Box, Button, FormControl, Input } from "@mui/material";
+import { useForm, Controller } from 'react-hook-form';
+import axios from 'axios'
+
+type Name = {
+  name: string;
+}
 
 const SearchBar = () => {
-  const theme = useTheme()
+  const { control, handleSubmit } = useForm<Name>({
+    defaultValues: {
+      name: ''
+    }
+  });
 
+  const handleBtn = async (data: Name) => {
+    await axios.post('/api/hello',data)
+    console.log(data)
+  }
   return(
-    <Box sx={{ textAlign: 'center' }}>
+    <Box sx={{ textAlign: 'center'}}>
       <Box sx={{ display: 'flex', alignItems: 'center', position: 'relative', width: '50%', margin: 'auto' }}>
-        <Input
-          disableUnderline
-          sx={{
-            flex: 1,
-            backgroundColor: 'white',
-            borderColor: 'black',
-            height: '60px',
-            borderRadius: 5,
-            paddingRight: '50px', // Add some right padding to leave space for the button
-          }}
-          placeholder="구단주 명"
-        />
-        <Button
-          sx={{
-            backgroundColor: 'white',
-            position: 'absolute',
-            right: '5px',
-            top: '50%',
-            transform: 'translateY(-50%)',
-            color: 'black',
-          }}
-          variant="contained"
-          disableElevation
-        >
-          ㄱㄱ
-        </Button>
+        <form onSubmit={handleSubmit(handleBtn)} style={{width: '100%'}}>
+          <FormControl
+            sx={{
+              flex: 1,
+              backgroundColor: 'white',
+              borderColor: 'black',
+              height: '60px',
+              borderRadius: 5,
+              width: '100%'
+            }}
+          >
+            <Controller
+              name="name"
+              control={control}
+              defaultValue=""
+              render={({ field }) => (
+                <>
+                  <Input
+                    {...field}
+                    disableUnderline
+                    sx={{
+                      flex: 1,
+                      borderRadius: 5,
+                      paddingLeft: '15px',
+                    }}
+                    placeholder="구단주 명"
+                  />
+                  <Button
+                    sx={{
+                      backgroundColor: 'white',
+                      position: 'absolute',
+                      right: '5px',
+                      top: '50%',
+                      transform: 'translateY(-50%)',
+                      color: 'black',
+                    }}
+                    variant="contained"
+                    disableElevation
+                    type='submit'
+                  >
+                    ㄱㄱ
+                  </Button>
+                </>
+              )}
+            />
+          </FormControl>
+        </form>
       </Box>
     </Box>
   )
