698f8cffb508a3c3327532ea3b80031248b4caff
GiSeong Lee <cj8928@gmail.com>
2023-07-31 17:49:52 +0900

    헤더 로고 클릭시 홈페이지 라우팅

--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -1,9 +1,13 @@
 import { AppBar, Box, Container, Typography } from "@mui/material";
+import { useRouter } from 'next/router';
 
 const Header = () => {
+  const router = useRouter();
   return(
     <Box sx={{marginTop: '20px'}}>
-        <Typography variant="h2" sx={{margin: '20px'}}>
+        <Typography variant="h2" sx={{margin: '20px', cursor: 'pointer', width: 'fit-content'}}
+          onClick={()=>router.push('/')}
+        >
           Logo
         </Typography>
 
