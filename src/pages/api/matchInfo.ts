47aefe8ff36937918bd4134ce9d2c7925366897a
GiSeong Lee <cj8928@gmail.com>
2023-08-02 17:57:51 +0900

    유저 정보에 따른 매치 정보 가져오기

--- src/pages/api/matchInfo.ts
+++ src/pages/api/matchInfo.ts
@@ -1,18 +1,30 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import axios from 'axios'
 import type { NextApiRequest, NextApiResponse } from 'next'
+
 
 export default async function getMatchInfo(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  
+  if(req.method === 'GET')
   try{
-    const response = await axios.get(`https://static.api.nexon.co.kr/fifaonline4/latest/matchtype.json`)
+    const matchInfo= req.query.matchType;
+    const accessId = req.query.accessId
+    const response = await axios.get(`https://api.nexon.co.kr/fifaonline4/v1.0/users/${accessId}/matches?matchtype=${matchInfo}&offset=0&limit=100`,
+    {
+      headers:{
+        Authorization: process.env.FIFAONLINE_API_HEADER_KEY
+      }
+    })
     console.log(response.data)
     res.status(200).json(response.data)
+
   }
   catch(error){
     console.log(error)
+    res.status(500).json({ error: 'An error occurred while fetching data' });
+  }
+  else {
+    res.status(405).json({ error: 'Method not allowed' });
   }
 }
--- src/pages/record/index.tsx
+++ src/pages/record/index.tsx
@@ -1,17 +1,48 @@
 import RecordBlock from '@/component/RecordBlock';
+import { Button, ButtonGroup } from '@mui/material';
+import { useMutation, useQuery } from '@tanstack/react-query';
+import axios from 'axios';
 import { useRouter } from 'next/router'
-import { useEffect } from 'react'
-const Record = () => {
+import { useEffect, useState } from 'react'
 
+const Record = () => {
   const router = useRouter();
-  console.log(router.query.name)
+  const [matchType, setMatchType] = useState<number>(50)
+  const { isLoading, isSuccess, data } = useQuery(['matchType', matchType], ()=>axios.get('/api/matchInfo',{
+    params:{
+      matchType: matchType,
+      accessId: router.query.accessId
+    }
+  })
+  )
+  console.log(data?.data)
   useEffect(() => {
     if(!router.isReady) return;
     console.log(router.query,'🙆‍♀️ 콘솔에 쿼리 찍힘!')
   }, [router.isReady])
+  
+
+  const handleMatchBtn = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    let text = e.currentTarget.innerText
+    switch(text){
+      case '공식경기':
+        setMatchType(50)
+        break;
+      case '친선경기':
+        setMatchType(40)
+        break;
+      case '감독모드':
+        setMatchType(52)
+        break;
+    }
+  }
   return(
     <>
-      hi
+      <ButtonGroup variant='contained' aria-label='outlined primary button group'>
+        <Button className='typeBtn' onClick={handleMatchBtn}>공식경기</Button>
+        <Button className='typeBtn' onClick={handleMatchBtn}>친선경기</Button>
+        <Button className='typeBtn' onClick={handleMatchBtn}>감독모드</Button>
+      </ButtonGroup>
       <RecordBlock/>
     </>
   )
